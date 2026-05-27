43804ee4c565d4582d5d2f609dc0d312b1823cd7
Logan Jones <loganjones110613@gmail.com>
2026-05-27 16:56:20 -0400

    created basic help menu

--- src/pyhilasm/pyhilasm.py
+++ src/pyhilasm/pyhilasm.py
@@ -1,4 +1,17 @@
+#!/bin/python
 import sys
 
+
+def command_line_utils():
+    if sys.argv[1] == "help":
+        print("""usage: ./pyhilasm.py [args] program
+cx86 - Compiles program according to the x86 ISA""")
+    else:
+        print("Not implemented/Not found")
+
 if __name__ == "__main__":
-    print("Hello World!")
+    if len(sys.argv) < 2:
+        print("No arguments provided. Try running `help`")
+        exit(1)
+    else:
+        command_line_utils()
