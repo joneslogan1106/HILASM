69bd2c3ac794c14167427ee79b0ff53b6b34a81c
Logan Jones <loganjones110613@gmail.com>
2026-05-27 17:04:33 -0400

    Added full project

--- src/pyhilasm/pyhilasm.py
+++ src/pyhilasm/pyhilasm.py
@@ -6,8 +6,10 @@ def command_line_utils():
     if sys.argv[1] == "help":
         print("""usage: ./pyhilasm.py [args] program
 cx86 - Compiles program according to the x86 ISA""")
+        exit(0)
     else:
         print("Not implemented/Not found")
+        exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
